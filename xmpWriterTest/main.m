2e6c51b15597736ac006510a5504971e616180f3
Douglas Carmichael <dcarmich@Douglass-Mac-Pro.local>
2015-01-18 06:53:12 -0600

    Initial Commit

---
+++ xmpWriterTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  xmpWriterTest
+//
+//  Created by Douglas Carmichael on 1/18/15.
+//  Copyright (c) 2015 Douglas Carmichael. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
